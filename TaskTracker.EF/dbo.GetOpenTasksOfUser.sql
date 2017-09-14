8fa07e0f10d8c9a87f1042c9735e6d504149bda5
Serge Volkov <serge.volk.4102@gmail.com>
2017-09-14 18:46:23 +0300

    Added stored procedures

---
+++ TaskTracker.EF/dbo.GetOpenTasksOfUser.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[GetOpenTasksOfUser]
+	@userId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    SELECT *
+    FROM [dbo].TaskSet as tasks
+    WHERE tasks.[Status] = 0 AND tasks.[Assignee_Id] = @userId
+END
---
+++ TaskTracker.EF/dbo.SetTaskStatus.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[SetTaskStatus]
+	@taskId int,
+	@newStatus int
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    Update [dbo].[TaskSet]
+    SET [Status] = @newStatus
+    where [Id] = @taskId
+END
